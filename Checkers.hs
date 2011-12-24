0a92456c9fa442f045d729ac1764ff441e8a303a
Jamie Ly <jamie.ly@gmail.com>
2011-12-24 15:34:48 -0500

    Changed code to use map to represent board

--- Checkers.hs
+++ Checkers.hs
@@ -1,67 +1,65 @@
 module Checkers ( Game ) where
 
 import Test.HUnit
+import Data.Map
 
 type Player = Marker
 
 type Size = (Int, Int) -- Width, Height
 type Position = (Int, Int) -- row, col
+type PositionMap = Map Position Marker
 
 data Game = Game GameState [Player] deriving (Show)
-data Board = Board Size [[Marker]] deriving (Show, Eq) -- Width, Height, Markers
+data Board = Board Size PositionMap deriving (Show, Eq) -- Width, Height, Markers
 data GameState = GameState Board Player deriving (Show, Eq) -- State, Board, Current Player
-data Marker = None | Black | Red deriving (Show, Eq)
+data Marker = None | Black | Red | King Marker deriving (Show, Eq)
 
 -- | Returns a list of Markers representing a board
 getBoard :: Size -> Board
 getBoard sz@(width, height) = board where
   board = Board sz markers
-  markers = take height $ repeat markerRow
-  markerRow = take width $ repeat None
+  positions = [(row, col) | row <- [1..height], col <- [1..width]]
+  markers = fromList $ zip positions (repeat None) :: PositionMap
 
 testGetBoard :: Test
 testGetBoard = "getBoard" ~: TestList [
-  markers (getBoard (3, 2)) ~?= [[None, None, None], [None, None, None]]
-  ]
+  markers (getBoard (3, 2)) ~?= fromList boardList
+  ] where
+  p :: Int -> Int -> (Position, Marker)
+  p r c = ((r,c), None)
+  boardList = [p 1 1, p 1 2, p 1 3, p 2 1, p 2 2, p 2 3]
 
 size :: Board -> Size
 size (Board s _) = s
 
 -- | Retrieve the markers of a board
-markers :: Board -> [[Marker]]
+markers :: Board -> PositionMap
 markers (Board _ ms) = ms
 
 -- | Update the markers associated with a board
-swapMarkers :: Board -> [[Marker]] -> Board
+swapMarkers :: Board -> PositionMap -> Board
 swapMarkers (Board sz _) ms = Board sz ms
 
 -- | Use to update the mark on the board
 updateBoard :: Board -> Position -> Marker -> Board
-updateBoard board (row, col) mark = newBoard where
+updateBoard board pos@(row, col) mark = newBoard where
   ms = markers board
   newBoard = swapMarkers board newMarks
-  newMarks = upR row col ms mark
-
-  upR :: Int -> Int -> [[Marker]] -> Marker -> [[Marker]]
-  upR r c (m:ns) mrk
-    | r == 0    = (upC c m mrk):ns
-    | otherwise = m : upR (r-1) c ns mrk
-  upR _ _ [] _ = [] 
-
-  upC :: Int -> [Marker] -> Marker -> [Marker]
-  upC c (mi:mis) mrk
-    | c == 0    = mrk : mis
-    | otherwise = mi : upC (c-1) mis mrk
-  upC _ [] _ = []
+  newMarks = insert pos mark ms
 
 testUpdateBoard :: Test
 testUpdateBoard = "updateBoard" ~: TestList [
-  markers origBoard ~?= [[None, None, None], [None, None, None]],
-  updateBoard origBoard (0,1) Black ~?= 
-    Board sz [[None, Black, None], [None, None, None]]
+  markers origBoard ~?= fromList boardList,
+  updateBoard origBoard (1,2) Black ~?= 
+    Board sz (fromList newBoardList)
   ] where
   sz = (3,2)
   origBoard = getBoard sz
+  p :: Int -> Int -> (Position, Marker)
+  p r c = ((r,c), None)
+  boardList = [p 1 1, p 1 2, p 1 3, p 2 1, p 2 2, p 2 3]
+  newBoardList = [p 1 1, ((1,2), Black), p 1 3, p 2 1, p 2 2, p 2 3]
+
 
 -- | Updates the game state by making a move at the 
 -- passed position
@@ -84,7 +82,6 @@ testUpdateState = "updateState" ~: TestList [
   sz = (3, 2)
   origBoard = getBoard sz
   newBoard = updateBoard origBoard pos Black
-
 
 test :: IO ()
 test = do
