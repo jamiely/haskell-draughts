2b8dba7b084e4da0d7f36b8a65db2cb67a79b3c8
Jamie Ly <me@jamie.ly>
2012-11-09 19:25:34 -0500

    Added check for valid moves before executing move

--- Main.hs
+++ Main.hs
@@ -6,18 +6,26 @@ playGame game = do
   if not (gameOver game) 
     then continueGame
     else finishGame where
+  possibleMoves = getGameMoves game
+  isValidMove move = move `elem` possibleMoves
   continueGame = do
     putStrLn $ show game
-    putStrLn $ "Possible moves: \n " ++ (show $ getGameMoves game) ++ "\nEnter your move as \"Move (src_col, src_row) (des_col, des_row)\" (q to quit): "
+    putStrLn $ "Possible moves: \n " ++ (show $ possibleMoves) ++ "\nEnter your move as \"Move (src_col, src_row) (des_col, des_row)\" (q to quit): "
     strMove <- getLine
-    if strMove /= "q" then playGame $ applyMove game strMove else putStrLn "Quitting game..." where
+    if strMove == "q"
+      then putStrLn "Quitting game..." 
+      else if isValidMove (getMove strMove)
+        then playGame $ applyMove game strMove 
+        else putStrLn $ strMove ++ " is not a valid move"
   finishGame = do 
     putStrLn $ "The game is over. Final game state: \n" ++ show game
     putStrLn $ show (gameWinner game) ++ " has won"
 
+getMove :: String -> Move
+getMove strMove = read strMove :: Move
+
 applyMove :: Game -> String -> Game
-applyMove game strMove = makeMove game move where
-	move = read strMove :: Move
+applyMove game strMove = makeMove game $ getMove strMove
 
 main :: IO ()
 main = do
